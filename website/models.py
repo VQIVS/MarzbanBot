4e76d29ba29a2851d19cadfdb9f9c589f4195ae8
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-10-15 21:42:41 +0330

    Create models
    create models for website.

--- website/models.py
+++ website/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Configuration(models.Model):
+    """This is a model for bot configurations"""
+    bot_name = models.CharField(max_length=255)
+    token = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
+
+
+class TelegramChannel(models.Model):
+    """This is a model for user telegram channels"""
+    name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+class Tutorial(models.Model):
+    """This is a model for handling the tutorials of bot """
+    name = models.CharField(max_length=255)
+    text = models.TextField(max_length=2043)
+
+class ChannelAdmin(models.Model):
+    name = models.CharField(max_length=255)
+    telegram_id = models.CharField(max_length=255)
