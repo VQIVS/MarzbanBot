c7f08cacb6d8668149474cfddd8c6ed15a033ec9
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-10-16 17:14:10 +0330

    Update models.py
    create models for the bot.

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from website.models import Product
+
+
+class Email(models.Model):
+    address = models.EmailField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.address
+
+
+class User(models.Model):
+    """A class for User objects"""
+    user_id = models.IntegerField()
+    primary_email = models.EmailField(max_length=255)
+    emails = models.ManyToManyField('Email', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.user_id)
+
+
+class Order(models.Model):
+    """"A class for Order objects"""
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders_by_user")
+    email = models.ForeignKey(Email, on_delete=models.CASCADE, related_name="orders_by_email")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE())
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.email
