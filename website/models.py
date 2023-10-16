bdb0dc7816ba47d262bfded0058d833ac608e492
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-10-16 15:12:35 +0330

    Update models.py
    add __str__ for classes

--- website/models.py
+++ website/models.py
@@ -6,10 +6,16 @@ class Configuration(models.Model):
     bot_name = models.CharField(max_length=255)
     token = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.bot_name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class TelegramChannel(models.Model):
@@ -18,11 +24,22 @@ class TelegramChannel(models.Model):
     title = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+
 class Tutorial(models.Model):
     """This is a model for handling the tutorials of bot """
     name = models.CharField(max_length=255)
     text = models.TextField(max_length=2043)
 
+    def __str__(self):
+        return self.name
+
+
 class ChannelAdmin(models.Model):
     name = models.CharField(max_length=255)
     telegram_id = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
