1dcbd117fdc196bad645344cc95da8c629a217bb
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-11-03 20:33:11 +0330

    Create run_bot.py
    create a management command to run the bot in django project.

---
+++ bot/management/commands/run_bot.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+from bot.handlers import bot
+
+class Command(BaseCommand):
+    help = 'Run the Telegram bot'
+
+    def handle(self, *args, **kwargs):
+        bot.infinity_polling()
