cbbbd6c7fd9d3be2dbb59ab40cdbbaf6923df45d
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-11-04 23:25:20 +0330

    Update urls.py
    create a url pattern for login page.

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from website.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
-    path('', include('bot.urls'))
+    path('', include('bot.urls')),
+    path('login', login, name='login'),
 ]
