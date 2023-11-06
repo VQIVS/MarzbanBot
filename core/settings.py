2649a8dfbce99ef4dad4e32cbd0b25587fa2fb84
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-11-06 19:58:14 +0330

    Update settings.py
    generate a secret_key

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+import secrets
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default=str(secrets.token_hex(50)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
