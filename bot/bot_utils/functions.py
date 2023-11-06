866b16d3bfd730783d0b83b19376ad75e3d6dfec
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-11-06 20:00:25 +0330

    Create functions.py
    create util functions.

---
+++ bot/bot_utils/functions.py
@@ -0,0 +1,5 @@
+import re
+def is_valid_email(email):
+    # Use a regular expression to validate the email format
+    email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(email_pattern, email)
