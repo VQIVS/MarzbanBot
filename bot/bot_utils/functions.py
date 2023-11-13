8bd766dc547291c532d00d64f3d3a954194917d0
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-11-13 15:30:45 +0330

    Delete files
    delet files

--- bot/bot_utils/functions.py
+++
@@ -1,5 +0,0 @@
-import re
-def is_valid_email(email):
-    # Use a regular expression to validate the email format
-    email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    return re.match(email_pattern, email)
