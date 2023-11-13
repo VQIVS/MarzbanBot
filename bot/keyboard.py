236ff1254d2ca02c2f1744e0ead0572554098113
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-11-13 15:31:33 +0330

    Update keyboard.py
    some changes

--- bot/keyboard.py
+++ bot/keyboard.py
@@ -1,6 +1,5 @@
 from telebot import types
-from website.models import Product
-
+from website.models import Product, Tutorial
 
 keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 buttons_row1 = [
@@ -10,7 +9,6 @@ buttons_row2 = [
     types.KeyboardButton('💬پشتیبانی'),
     types.KeyboardButton('📚راهنما اتصال'),
     types.KeyboardButton('اضافه کردن ایمیل')
-
 ]
 buttons_row3 = [
     types.KeyboardButton('اشتراک های من')
@@ -26,8 +24,13 @@ product_2_name = Product.objects.get(pk=2)
 product_3_name = Product.objects.get(pk=3)
 product_4_name = Product.objects.get(pk=4)
 
-product_1 = types.InlineKeyboardButton(product_1_name, callback_data="product_1")
-product_2 = types.InlineKeyboardButton(product_2_name, callback_data="product_2")
-product_3 = types.InlineKeyboardButton(product_3_name, callback_data="product_3")
-product_4 = types.InlineKeyboardButton(product_4_name, callback_data="product_4")
+product_1 = types.InlineKeyboardButton(product_1_name.name, callback_data="product_1")
+product_2 = types.InlineKeyboardButton(product_2_name.name, callback_data="product_2")
+product_3 = types.InlineKeyboardButton(product_3_name.name, callback_data="product_3")
+product_4 = types.InlineKeyboardButton(product_4_name.name, callback_data="product_4")
 products_keyboard.add(product_1, product_2, product_3, product_4)
+
+tutorial = Tutorial.objects.get(pk=1)
+channel_id = types.InlineKeyboardButton("آموزش ها", callback_data="tt", url=tutorial.telegram_id)
+tutorial_keyboard = types.InlineKeyboardMarkup(row_width=1)
+tutorial_keyboard.add(channel_id)
