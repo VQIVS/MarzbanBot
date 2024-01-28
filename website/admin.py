40cbf8024d058b9c20bf4c863a9106c81fd4a192
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-01-28 21:22:14 +0330

    Update admin.py
    register Message model.

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Configuration, Product, TelegramChannel, Tutorial, ChannelAdmin
+from .models import Configuration, Product, TelegramChannel, Tutorial, ChannelAdmin, Message
 
 
 admin.site.register(Configuration)
@@ -7,4 +7,5 @@ admin.site.register(Product)
 admin.site.register(TelegramChannel)
 admin.site.register(Tutorial)
 admin.site.register(ChannelAdmin)
+admin.site.register(Message)
 
