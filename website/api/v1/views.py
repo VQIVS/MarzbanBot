38336c35900927d9de9f0978e2aa2db74d4d73af
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-01-28 21:12:48 +0330

    Update views.py
    create a view for the Message.

--- website/api/v1/views.py
+++ website/api/v1/views.py
@@ -1,26 +1,34 @@
 from ...models import Configuration, Product, TelegramChannel, Tutorial, ChannelAdmin
 from rest_framework.viewsets import ModelViewSet
 from .serializers import ConfigurationSerializer, ProductSerializer, TelegramChannelSerializer, TutorialSerializer, \
-    ChannelAdminSerializer
+    ChannelAdminSerializer, MessageSerializer
 
 
 class ConfigurationModelViewSet(ModelViewSet):
     queryset = Configuration.objects.all()
     serializer_class = ConfigurationSerializer
 
+
 class ProductModelViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
 
 class TelegramChannelModelViewSet(ModelViewSet):
     queryset = TelegramChannel.objects.all()
     serializer_class = TelegramChannelSerializer
 
+
 class TutorialModelViewSet(ModelViewSet):
     queryset = Tutorial.objects.all()
     serializer_class = TutorialSerializer
+
 
 class ChannelAdminModelViewSet(ModelViewSet):
     queryset = ChannelAdmin.objects.all()
     serializer_class = ChannelAdminSerializer
 
+
+class MessageModelViewSet(ModelViewSet):
+    queryset = ChannelAdmin.objects.all()
+    serializer_class = MessageSerializer
