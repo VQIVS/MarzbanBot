7167f57ed0bc74f007c68c9e122cfe957f6fa266
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-01-28 20:27:34 +0330

    Delete files
    delete useless files for v2.

--- bot/handlers.py
+++ bot/handlers.py
@@ -2,11 +2,9 @@ from sqlite3 import DatabaseError
 
 from telebot import TeleBot
 from website.models import Configuration, ChannelAdmin
-from bot.models import User, Email
 import os
 import django
-from bot.keyboard import keyboard, products_keyboard, tutorial_keyboard
-from .utils.functions import is_valid_email
+from bot.keyboard import keyboard
 
 configuration = Configuration.objects.first()
 channel_admin = ChannelAdmin.objects.get(pk=1)
@@ -26,55 +24,3 @@ def start(message):
     text = "لطفا قبل از خرید با استفاده از دکمه اضافه کردن ایمیل , ایمیل خود را اضافه کنید و سپس اقدام به خرید بفرمایید"
     bot.send_message(user_id, text)
 
-
-@bot.message_handler(func=lambda message: message.text == 'اضافه کردن ایمیل')
-def add_email(message):
-    user_id = message.from_user.id
-    address = str(user_id) + '@telegram.com'
-    user, created = User.objects.get_or_create(user_id=str(user_id))
-    user.primary_email = address
-    user.save()
-    bot.send_message(user_id, "لطفا ایمیل خود را به صورت صحیح وارد کنید")
-
-
-@bot.message_handler(func=lambda message: message.text == '⭐️خرید سرویس')
-def buy(message):
-    user_id = message.from_user.id
-    message_buy = "🛒 لطفاً یکی از پلن های زیر را برای خرید انتخاب کنید."
-    bot.send_message(user_id, message_buy, reply_markup=products_keyboard)
-
-
-@bot.message_handler(func=lambda message: message.text == "💬پشتیبانی")
-def support(message):
-    user_id = message.from_user.id
-    support_id = channel_admin.telegram_id
-    bot.send_message(user_id, support_id)
-
-
-@bot.message_handler(func=lambda message: message.text == "اشتراک های من")
-def subscriptions(message):
-    user_id = message.from_user.id
-
-
-@bot.message_handler(func=lambda message: is_valid_email(message.text))
-def add_email(message):
-    user_id = message.chat.id
-    email = message.text
-    try:
-        user, created = User.objects.get_or_create(user_id=str(user_id))
-
-        email_obj, created = Email.objects.get_or_create(address=email)
-        user.emails.add(email_obj)
-
-        message_save = f'کاربر {email} ثبت شد'
-        bot.send_message(user_id, message_save)
-    except DatabaseError:
-        message_unsaved = 'ایمیل تکراری یا اشتباه است.'
-        bot.send_message(user_id, message_unsaved)
-
-
-@bot.message_handler(func=lambda message: message.text == "📚راهنما اتصال")
-def tutorial(message):
-    user_id = message.chat.id
-    text = "برای آموزش ها عضو شوید"
-    bot.send_message(user_id, text, reply_markup=tutorial_keyboard)
--- bot/keyboard.py
+++ bot/keyboard.py
@@ -1,5 +1,4 @@
 from telebot import types
-from website.models import Product, Tutorial
 
 keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 buttons_row1 = [
@@ -17,20 +16,4 @@ keyboard.add(*buttons_row2)
 keyboard.add(*buttons_row3)
 
 
-products_keyboard = types.InlineKeyboardMarkup(row_width=1)
-product_1_name = Product.objects.get(pk=1)
-product_2_name = Product.objects.get(pk=2)
-product_3_name = Product.objects.get(pk=3)
-product_4_name = Product.objects.get(pk=4)
 
-product_1 = types.InlineKeyboardButton(product_1_name.name, callback_data=product_1_name.id)
-product_2 = types.InlineKeyboardButton(product_2_name.name, callback_data=product_2_name.id)
-product_3 = types.InlineKeyboardButton(product_3_name.name, callback_data=product_3_name.id)
-product_4 = types.InlineKeyboardButton(product_4_name.name, callback_data=product_4_name.id)
-products_keyboard.add(product_1, product_2, product_3, product_4)
-
-
-tutorial = Tutorial.objects.get(pk=1)
-channel_id = types.InlineKeyboardButton("آموزش ها", callback_data="tt", url=tutorial.telegram_id)
-tutorial_keyboard = types.InlineKeyboardMarkup(row_width=1)
-tutorial_keyboard.add(channel_id)
