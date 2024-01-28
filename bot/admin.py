0101b9a64b598157a518cbc3e50fb5fd10c7760e
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-01-28 20:30:09 +0330

    Update admin.py
    modify admin for registering the models.

--- bot/admin.py
+++ bot/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Email, User, Order
+from .models import User, Order, Subscription
 
-admin.site.register(Email)
 admin.site.register(User)
 admin.site.register(Order)
+admin.site.register(Subscription)
