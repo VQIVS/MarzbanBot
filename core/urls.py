088b0765fb5251a4c3c6c36600d15896839dbb79
farnamah <145441216+farnamah@users.noreply.github.com>
2024-01-30 17:04:59 +0330

    Removing Redoc
    i remove redoc from core urls

--- core/urls.py
+++ core/urls.py
@@ -45,6 +45,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-docs/", include_docs_urls(title="api-sample")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
