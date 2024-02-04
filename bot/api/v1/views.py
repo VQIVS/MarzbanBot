77b160dc4b58331addbab58b93c6942495f4049d
farnamah <145441216+farnamah@users.noreply.github.com>
2024-02-04 17:16:02 +0330

    API swagger
    adding token to api

--- bot/api/v1/views.py
+++ bot/api/v1/views.py
@@ -1,6 +1,7 @@
-from .serializers import BotOrderSerializer, BotOrderSerializer
+from .serializers import BotOrderSerializer, BotOrderSerializer, CustomAuthTokenSerializer
 from rest_framework import viewsets
 from ...models import User, Order
+from rest_framework.authtoken.views import ObtainAuthToken
 
 # from ...models import Email, User, Order
 """ commented for v1 """
@@ -19,6 +20,22 @@ class UserModelViewSet(viewsets.ModelViewSet):
 #     serializer_class = EmailSerializer
 #
 
+
 class OrderModelViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = BotOrderSerializer
+
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    serializer_class = CustomAuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key, "user_id": user.pk, "email": user.email})
+
+
