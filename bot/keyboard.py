4bf754bf27a28aa6eeba71837d89a5cec7e1483f
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-02-07 01:34:29 +0330

    Update keyboard.py
    create a tutorial keyboard.

--- bot/keyboard.py
+++ bot/keyboard.py
@@ -1,5 +1,6 @@
 from telebot import types
 from bot.models import Product
+from website.models import Tutorial
 
 keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 buttons_row1 = [
@@ -16,9 +17,10 @@ keyboard.add(*buttons_row1)
 keyboard.add(*buttons_row2)
 keyboard.add(*buttons_row3)
 
-products = Product.objects.all()
 
-# Create a list of inline buttons
+""" Create a list of inline buttons for products """
+
+products = Product.objects.all()
 inline_buttons = []
 for i, product in enumerate(products, start=1):
 
@@ -30,4 +32,22 @@ for i, product in enumerate(products, start=1):
 inline_keyboard_markup = types.InlineKeyboardMarkup()
 for button in inline_buttons:
     inline_keyboard_markup.add(button)
+
+""" Create a list of inline buttons for tutorials """
+tutorial = Tutorial.objects.all()
+inline_tutorial_buttons = []
+
+for i, tutorial in enumerate(tutorial, start=1):
+    tutorial_text = tutorial.name
+    telegram_id_url = tutorial.telegram_id
+
+    inline_tutorial_button = types.InlineKeyboardButton(text=tutorial_text, callback_data=tutorial_text, url=telegram_id_url)
+    inline_tutorial_buttons.append(inline_tutorial_button)
+
+inline_tutorial_markup = types.InlineKeyboardMarkup()
+for btn in inline_tutorial_buttons:
+    inline_tutorial_markup.add(btn)
+
+
+
 
