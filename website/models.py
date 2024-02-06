9cc3314e61ea20ed3d9f7042dbb9593cf16552c3
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-02-06 16:14:39 +0330

    Update models.py
    make the UserManager better and describe with comments.

--- website/models.py
+++ website/models.py
@@ -1,24 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.contrib.auth.models import AbstractUser
-from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 import secrets
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
         if not email:
-            raise ValueError('The Email field must be set')
+            raise ValueError(_("The Email must be set"))
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
 
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
         return self.create_user(email, password, **extra_fields)
 
 
@@ -116,13 +127,9 @@ class ChannelAdmin(models.Model):
 
 
 class Message(models.Model):
-    id = models.IntegerField(primary_key=True)
     subject = models.CharField(max_length=255)
     text = models.TextField()
 
     def __str__(self):
         return self.subject
 
-
-class CheckmigrationsModel(models.Model):
-    name = models.CharField(max_length=255)
