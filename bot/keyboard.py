d2ebeff433c341e7095ec4275ceb20a2cab8b32f
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-02-07 01:09:29 +0330

    Update keyboard.py
    create an inline key board markup for products.

--- bot/keyboard.py
+++ bot/keyboard.py
@@ -1,4 +1,5 @@
 from telebot import types
+from bot.models import Product
 
 keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 buttons_row1 = [
@@ -15,5 +16,18 @@ keyboard.add(*buttons_row1)
 keyboard.add(*buttons_row2)
 keyboard.add(*buttons_row3)
 
+products = Product.objects.all()
 
+# Create a list of inline buttons
+inline_buttons = []
+for i, product in enumerate(products, start=1):
+
+    button_text = product.name
+    callback_data = f"p_{i}"
+    inline_button = types.InlineKeyboardButton(text=button_text, callback_data=callback_data)
+    inline_buttons.append(inline_button)
+
+inline_keyboard_markup = types.InlineKeyboardMarkup()
+for button in inline_buttons:
+    inline_keyboard_markup.add(button)
 
