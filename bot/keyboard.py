97a77f1ae431e2d395cda5009233c66ac13101a7
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-02-07 15:06:27 +0330

    Update keyboard.py
    fix : tutorial callback_queries

--- bot/keyboard.py
+++ bot/keyboard.py
@@ -17,15 +17,15 @@ keyboard.add(*buttons_row1)
 keyboard.add(*buttons_row2)
 keyboard.add(*buttons_row3)
 
-
 """ Create a list of inline buttons for products """
+product_callbacks = []
 
 products = Product.objects.all()
 inline_buttons = []
 for i, product in enumerate(products, start=1):
-
     button_text = product.name
     callback_data = f"p_{i}"
+    product_callbacks.append(callback_data)
     inline_button = types.InlineKeyboardButton(text=button_text, callback_data=callback_data)
     inline_buttons.append(inline_button)
 
@@ -34,6 +34,7 @@ for button in inline_buttons:
     inline_keyboard_markup.add(button)
 
 """ Create a list of inline buttons for tutorials """
+
 tutorial = Tutorial.objects.all()
 inline_tutorial_buttons = []
 
@@ -41,7 +42,8 @@ for i, tutorial in enumerate(tutorial, start=1):
     tutorial_text = tutorial.name
     telegram_id_url = tutorial.telegram_id
 
-    inline_tutorial_button = types.InlineKeyboardButton(text=tutorial_text, callback_data=tutorial_text, url=telegram_id_url)
+    inline_tutorial_button = types.InlineKeyboardButton(text=tutorial_text, callback_data=tutorial_text,
+                                                        url=telegram_id_url)
     inline_tutorial_buttons.append(inline_tutorial_button)
 
 inline_tutorial_markup = types.InlineKeyboardMarkup()
