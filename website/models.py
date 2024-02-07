bddad940acee4e6deae51aa539dc3cdda7378bbd
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-02-08 01:40:18 +0330

    Update models.py

--- website/models.py
+++ website/models.py
@@ -92,7 +92,7 @@ class Configuration(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
