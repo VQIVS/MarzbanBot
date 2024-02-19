fdf92ca0b5a25b3cbd00acae5eb81f23c887b9db
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-02-19 20:14:48 +0330

    Update models.py
    create payment model

--- website/models.py
+++ website/models.py
@@ -135,11 +135,16 @@ class Message(models.Model):
 
 class PaymentMethod(models.Model):
     holders_name = models.CharField(max_length=255)
-    trx_address = models.CharField(max_length=255, blank=True)
-    usdt_address = models.CharField(max_length=255, blank=True)
     card_number = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.holders_name
 
+class Payment(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    photo = models.ImageField(upload_to='bot/payment_photos/')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Payment of {self.amount} made at {self.timestamp}"
 
