c0c853100e05df4321c7b9dc0d47325554dd4f7a
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-02-19 20:28:36 +0330

    Update views.py
    delete token linse.

--- website/api/v1/views.py
+++ website/api/v1/views.py
@@ -1,10 +1,7 @@
 from ...models import Configuration, Product, TelegramChannel, Tutorial, ChannelAdmin, User
 from rest_framework.viewsets import ModelViewSet
 from .serializers import ConfigurationSerializer, ProductSerializer, TelegramChannelSerializer, TutorialSerializer, \
-    ChannelAdminSerializer, MessageSerializer, WebsiteUserSerializer, CustomAuthTokenSerializer, PaymentMethodSerializer
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
+    ChannelAdminSerializer, MessageSerializer, WebsiteUserSerializer, PaymentMethodSerializer
 
 
 class UserModelViewSet(ModelViewSet):
@@ -40,19 +37,6 @@ class ChannelAdminModelViewSet(ModelViewSet):
 class MessageModelViewSet(ModelViewSet):
     queryset = ChannelAdmin.objects.all()
     serializer_class = MessageSerializer
-
-
-class CustomObtainAuthToken(ObtainAuthToken):
-    serializer_class = CustomAuthTokenSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(
-            data=request.data, context={"request": request}
-        )
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data["user"]
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({"token": token.key, "user_id": user.pk, "email": user.email})
 
 
 class PaymentMethodModelViewSet(ModelViewSet):
