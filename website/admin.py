27d6ecf0f49198ac6556efda85af7961e0620246
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-02-19 20:13:50 +0330

    Update admin.py
    register model.

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Configuration, Product, TelegramChannel, Tutorial, ChannelAdmin, Message, User, PaymentMethod
+from .models import Configuration, Product, TelegramChannel, Tutorial, ChannelAdmin, Message, User, PaymentMethod, Payment
 
 admin.site.register(User)
 admin.site.register(Configuration)
@@ -9,3 +9,4 @@ admin.site.register(Tutorial)
 admin.site.register(ChannelAdmin)
 admin.site.register(Message)
 admin.site.register(PaymentMethod)
+admin.site.register(Payment)
