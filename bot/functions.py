5858e8a8b6cfdf24f71098260ed59e5231db8e6b
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-02-20 23:59:40 +0330

    Update functions.py
    reformat & lint

--- bot/functions.py
+++ bot/functions.py
@@ -5,6 +5,19 @@ import random
 
 
 def create_user(username, data_limit, expire, access_token, api_url):
+    """
+    Create a new user with the provided parameters.
+
+    Args:
+        username (str): The username of the new user.
+        data_limit (float): The data limit for the new user.
+        expire (int): The expiration date for the new user.
+        access_token (str): The access token for API authentication.
+        api_url (str): The URL of the API endpoint.
+
+    Returns:
+        dict: The response JSON containing user information if successful, None otherwise.
+    """
     url = f"{api_url}/api/user"
 
     payload = {
@@ -35,14 +48,22 @@ def create_user(username, data_limit, expire, access_token, api_url):
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
-        print(f"Error occurred while creating user {username}: {e}")
+        logging.error(f"Error occurred while creating user {username}: {e}")
         return None
 
 
-logging.basicConfig(filename='script_log.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
-
-
 def get_access_token(username, password, api_url):
+    """
+    Obtain the access token for API authentication.
+
+    Args:
+        username (str): The username for authentication.
+        password (str): The password for authentication.
+        api_url (str): The URL of the API endpoint.
+
+    Returns:
+        str: The access token if successful, None otherwise.
+    """
     url = f"{api_url}/api/admin/token"
     data = {
         'username': username,
@@ -57,12 +78,34 @@ def get_access_token(username, password, api_url):
         logging.error(f'Error occurred while obtaining access token: {e}')
         return None
 
+
 def generate_custom_id(length):
+    """
+    Generate a custom ID with the specified length.
+
+    Args:
+        length (int): The length of the custom ID.
+
+    Returns:
+        str: The generated custom ID.
+    """
     random_number = str(random.getrandbits(256))  # Generate a random 256-bit number
     hashed = hashlib.sha256(random_number.encode()).hexdigest()  # Hash the random number
     return hashed[:length]  # Return the first `length` characters
 
+
 def get_user(username, access_token, api_url):
+    """
+    Get user information for the specified username.
+
+    Args:
+        username (str): The username of the user to retrieve information for.
+        access_token (str): The access token for API authentication.
+        api_url (str): The URL of the API endpoint.
+
+    Returns:
+        dict: The response JSON containing user information if successful, None otherwise.
+    """
     url = f"{api_url}/api/user/{username}"
     headers = {
         'accept': 'application/json',
@@ -74,7 +117,5 @@ def get_user(username, access_token, api_url):
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
-        print(f"Error occurred while getting user {username}: {e}")
+        logging.error(f"Error occurred while getting user {username}: {e}")
         return None
-
-
