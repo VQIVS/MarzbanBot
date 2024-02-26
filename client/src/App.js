ed47a04b84d788d54143070e0912f795e97b95d7
AidaNiknejad <140331255+AidaNiknejad@users.noreply.github.com>
2024-02-26 20:07:59 +0330

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,20 @@
-import './Login.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import Dashboard from './Dashboard';
 function App() {
     return (
-        <form className="wrapper">
-            <h2>LOGIN</h2>
-            <section className="group">
-                <input
-                    type="text"
-                    size="30"
-                    className="input"
-                    name="email"
-                    required
-                />
-                <label htmlFor="email" className="label">
-                    Email
-                </label>
-            </section>
-            <section className="group">
-                <input
-                    type="password"
-                    minLength="8"
-                    className="input"
-                    name="password"
-                    required
-                />
-                <label htmlFor="password" className="label">
-                    Password
-                </label>
-            </section>
-            <button type="button" className="btn">
-                LOGIN
-            </button>
-            <span className="footer"></span>
-        </form>
+       
+        <Router>
+             <Routes>
+             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </Router>
+       
     );
 }
+
 export default App;
