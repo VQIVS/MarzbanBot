198a4ece19c216d451838a81e26b5e2e265615c3
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-02-28 15:22:58 +0330

    Update urls.py
    adding app_name into urls.py

--- core/bot/urls.py
+++ core/bot/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
+app_name = 'api-v1'
+
 urlpatterns = [
-    path("", include("django.contrib.auth.urls")),
     path("api/v1/", include("bot.api.v1.urls")),
 ]
