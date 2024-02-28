5234930b61c852f93d7458ca982548a9d5aebf18
AidaNiknejad <140331255+AidaNiknejad@users.noreply.github.com>
2024-02-28 19:41:40 +0330

    dashboard.js
    create users form

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Sidebar from './Sidebar';
-import MainContent from './MainContent';
-import './Dashboard.css';
-
+import './Dashboard.css'; 
 function Dashboard() {
-  return (
-    <Router>
-      <div className="dashboard">
-        <Sidebar />
-        <Routes>
-          <Route path="/" exact component={Home} />
-          <Route path="/analytics" component={Analytics} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
-function Home() {
-  return <MainContent title="Home" />;
-}
-
-function Analytics() {
-  return <MainContent title="Analytics" />;
+    return (
+        <div className="dashboard">
+           <form className="users">
+            <h2 className="usersh2">Users</h2>
+           </form>
+        </div>
+    );
 }
 
 export default Dashboard;
