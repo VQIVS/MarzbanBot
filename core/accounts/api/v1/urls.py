55b179151081f74040099ff1ec79cbf963a9acea
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-02-28 15:19:21 +0330

    Accounts app API
    create registration api for the accounts app.

---
+++ core/accounts/api/v1/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+app_name = 'api-v1'
+
+urlpatterns = [
+    path('registration/', views.RegistrationApiView.as_view(), name='registration' )
+
+]
