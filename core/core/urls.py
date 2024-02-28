00badd3eff60b44507cbd797c8c7002be79f5911
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-02-28 15:23:22 +0330

    Update urls.py
    add accounts app urls into core urls

--- core/core/urls.py
+++ core/core/urls.py
@@ -4,6 +4,7 @@ from rest_framework import permissions
 from rest_framework.documentation import include_docs_urls
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.generators import SchemaGenerator
 
 """
 URL configuration for core project.
@@ -52,6 +53,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("website.urls")),
     path("", include("bot.urls")),
+    path("accounts/", include("accounts.urls")),
+
     path("grappelli/", include("grappelli.urls")),
     path("api-docs/", include_docs_urls(title="api sample")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema_view'),
