c16744d98f930258caa75d52a508094ffc97f83b
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-02-28 15:24:22 +0330

    Update urls.py
    The default router is used to register viewsets for "user" and "order" endpoints.
    The router-generated URLs are included under the root path ("/") and also under the "bot/" path.
    Custom paths are added for retrieving all users ("/users/") and all orders ("/orders/"), each bound to their respective custom actions in the viewsets (get_all_users and get_all_orders).
    This configuration provides a clear separation between default CRUD operations and custom actions for retrieving all users and orders.

--- core/bot/api/v1/urls.py
+++ core/bot/api/v1/urls.py
@@ -2,9 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserModelViewSet, OrderModelViewSet
 
+app_name = "api-bot-v1"
+
 router = DefaultRouter()
 router.register("user", UserModelViewSet, basename="BotUser")
 router.register("order", OrderModelViewSet, basename="BotOrder")
 urlpatterns = [
-    path("", include(router.urls)),
+    path("bot/", include(router.urls)),
+    path("users/", UserModelViewSet.as_view({'get': 'get_all_users'}), name="user-get-all"),
+    path("orders/", OrderModelViewSet.as_view({'get': 'get_all_orders'}), name="order-get-all"),
 ]
