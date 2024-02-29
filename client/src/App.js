e482fe91cb585568eb2e2e94eec529332a5102c6
AidaNiknejad <140331255+AidaNiknejad@users.noreply.github.com>
2024-02-29 20:18:14 +0330

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './Login';
 import Dashboard from './Dashboard';
+
 function App() {
     return (
-       
         <Router>
-             <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="*" element={<Navigate to="/" />} />
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </Router>
-       
     );
 }
 
