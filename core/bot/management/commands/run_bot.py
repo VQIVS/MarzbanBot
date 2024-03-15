95ae9ef93c8a4dba6b79af18813454dd69075080
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-03-16 01:16:49 +0330

    Update run_bot.py
    fix: delete watchdog monitoring because of crashing.

--- core/bot/management/commands/run_bot.py
+++ core/bot/management/commands/run_bot.py
@@ -1,34 +1,13 @@
-from django.core.management.base import BaseCommand
 from bot.telegram.handlers.handler import bot
-import os
-import sys
-import time
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-
-
-class MyHandler(FileSystemEventHandler):
-    def __init__(self):
-        self.modified = False
-
-    def on_modified(self, event):
-        if not self.modified:
-            print("File modified. Reloading bot...")
-            self.modified = True
-            time.sleep(2)  # Add a small delay to prevent rapid restarts
-            os.execv(sys.executable, ["python"] + sys.argv)
+from django.core.management.base import BaseCommand
 
 
 def run_bot():
-    event_handler = MyHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path=".", recursive=True)
-    observer.start()
     try:
         bot.infinity_polling()
-    finally:
-        observer.stop()
-        observer.join()
+        print("Bot is running")
+    except KeyboardInterrupt:
+        pass  # Stop the bot gracefully if KeyboardInterrupt is received
 
 
 class Command(BaseCommand):
