88253586578624a907076b1d1747e67c90d58ace
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-03-17 17:43:37 +0330

    Update urls.py
    update: update urls for send messages.

--- core/bot/api/v1/urls.py
+++ core/bot/api/v1/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserModelViewSet, OrderModelViewSet
+from .views import UserModelViewSet, OrderModelViewSet, SendMessageToAllUsers
 
 app_name = "v1-bot"
 
@@ -13,4 +13,6 @@ urlpatterns = [
     path("bot/", include(router.urls)),
     path("users/", UserModelViewSet.as_view({'get': 'get_all_users'}), name="user-get-all"),
     path("orders/", OrderModelViewSet.as_view({'get': 'get_all_orders'}), name="order-get-all"),
+    path("send-message-to-all-users/", SendMessageToAllUsers.as_view(), name="send-message-to-all-users"),
+
 ]
