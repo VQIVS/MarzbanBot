7c1356ad5551ef100494c08204b5cb24df98b266
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-03-18 16:02:08 +0330

    Update urls.py

--- core/bot/urls.py
+++ core/bot/urls.py
@@ -5,5 +5,5 @@ app_name = "api-v1"
 
 urlpatterns = [
     path("api/v1/", include("bot.api.v1.urls")),
-    path("send-message-to-all-users/", send_message, name="send_message_to_all_users"),
+    path("send-message/", send_message, name="send_message_to_all_users"),
 ]
