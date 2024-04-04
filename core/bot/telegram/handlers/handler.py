26bb02b935fe4c3bb8369d2e53747d10864123ae
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-04-04 19:53:08 +0330

    Update handler.py

--- core/bot/telegram/handlers/handler.py
+++ core/bot/telegram/handlers/handler.py
@@ -74,98 +74,98 @@ def start(message):
         main_handler.start(message)
 
 
-# @bot.message_handler(func=lambda message: message.text == "💡 راهنما‌ی سرویس")
-# def tutorial(message):
-#     main_handler.tutorial(message)
-#
-#
-# @bot.message_handler(func=lambda message: message.text == "💬 پشتیبانی")
-# def support(message):
-#     main_handler.support(message)
-#
-#
-# @bot.message_handler(func=lambda message: message.text == "⭐️ خرید سرویس")
-# def service(message):
-#     main_handler.buy(message)
-#
-#
-# @bot.message_handler(func=lambda message: message.text == "🛍 خرید عمده️")
-# def whole_service(message):
-#     main_handler.whole_buy(message)
-#
-#
-# @bot.message_handler(func=lambda message: message.text == "🧪دریافت سرور تست")
-# def test_subscription(message):
-#     main_handler.test_subscription(message)
-#
-#
-# @bot.callback_query_handler(func=lambda query: query.data.startswith("p_"))
-# def create_invoice(query):
-#     order_handler.create_service_invoice(query)
-#
-#
-# @bot.callback_query_handler(func=lambda query: query.data == "confirm")
-# def confirm_order_invoice(query):
-#     order_handler.confirm_order(query)
-#
-#
-# @bot.callback_query_handler(func=lambda query: query.data == "trx")
-# def trx_purchase(query):
-#     purchase_handler.trx_purchase(query)
-#
-#
-# @bot.callback_query_handler(func=lambda query: query.data == "card")
-# def card_purchase(query):
-#     purchase_handler.card_purchase(query)
-#
-#
-# @bot.message_handler(func=lambda message: message.text == "👤 اشتراک‌های من")
-# def get_subscription_data(message):
-#     user_handler.get_user_data(message)
-#
-#
-# @bot.callback_query_handler(func=lambda query: query.data == "delete")
-# def delete_subscription(query):
-#     user_handler.delete_subscription(query)
-#
-#
-# @bot.callback_query_handler(func=lambda query: query.data.startswith("m_"))
-# def whole_service_selection(query):
-#     order_handler.whole_service_selection(query)
-#
-#
-# @bot.message_handler(
-#     func=lambda message: BotUser.objects.filter(
-#         user_id=message.chat.id, state="quantity_input"
-#     ).exists()
-# )
-# def create_invoice(message):
-#     order_handler.create_whole_service_invoice(message)
-#
-#
-# @bot.callback_query_handler(func=lambda query: query.data == "cancel")
-# def cancel_process(query):
-#     rollback(query)
-#
-#
-# @bot.channel_post_handler(content_types=["text"])
-# def accept_purchase(message):
-#     confirmation.accept_purchase(message)
-#
-#
-# @bot.message_handler(content_types=["photo"])
-# def send_photo(message):
-#     purchase_handler.send_order_invoice(message)
-#
-#
-# @bot.message_handler(func=lambda message: message.text == "👨‍👩‍👧‍👧 معرفی به دوستان")
-# def refer(message):
-#     main_handler.refer(message)
-#
-#
-# ignored_user_ids = [622491665]  # Add the user IDs you want to ignore here
-#
-#
-# @bot.message_handler(func=lambda message: message.from_user.id in ignored_user_ids)
-# def ignore_messages(message):
-#     pass
+@bot.message_handler(func=lambda message: message.text == "💡 راهنما‌ی سرویس")
+def tutorial(message):
+    main_handler.tutorial(message)
+
+
+@bot.message_handler(func=lambda message: message.text == "💬 پشتیبانی")
+def support(message):
+    main_handler.support(message)
+
+
+@bot.message_handler(func=lambda message: message.text == "⭐️ خرید سرویس")
+def service(message):
+    main_handler.buy(message)
+
+
+@bot.message_handler(func=lambda message: message.text == "🛍 خرید عمده️")
+def whole_service(message):
+    main_handler.whole_buy(message)
+
+
+@bot.message_handler(func=lambda message: message.text == "🧪دریافت سرور تست")
+def test_subscription(message):
+    main_handler.test_subscription(message)
+
+
+@bot.callback_query_handler(func=lambda query: query.data.startswith("p_"))
+def create_invoice(query):
+    order_handler.create_service_invoice(query)
+
+
+@bot.callback_query_handler(func=lambda query: query.data == "confirm")
+def confirm_order_invoice(query):
+    order_handler.confirm_order(query)
+
+
+@bot.callback_query_handler(func=lambda query: query.data == "trx")
+def trx_purchase(query):
+    purchase_handler.trx_purchase(query)
+
+
+@bot.callback_query_handler(func=lambda query: query.data == "card")
+def card_purchase(query):
+    purchase_handler.card_purchase(query)
+
+
+@bot.message_handler(func=lambda message: message.text == "👤 اشتراک‌های من")
+def get_subscription_data(message):
+    user_handler.get_user_data(message)
+
+
+@bot.callback_query_handler(func=lambda query: query.data == "delete")
+def delete_subscription(query):
+    user_handler.delete_subscription(query)
+
+
+@bot.callback_query_handler(func=lambda query: query.data.startswith("m_"))
+def whole_service_selection(query):
+    order_handler.whole_service_selection(query)
+
+
+@bot.message_handler(
+    func=lambda message: BotUser.objects.filter(
+        user_id=message.chat.id, state="quantity_input"
+    ).exists()
+)
+def create_invoice(message):
+    order_handler.create_whole_service_invoice(message)
+
+
+@bot.callback_query_handler(func=lambda query: query.data == "cancel")
+def cancel_process(query):
+    rollback(query)
+
+
+@bot.channel_post_handler(content_types=["text"])
+def accept_purchase(message):
+    confirmation.accept_purchase(message)
+
+
+@bot.message_handler(content_types=["photo"])
+def send_photo(message):
+    purchase_handler.send_order_invoice(message)
+
+
+@bot.message_handler(func=lambda message: message.text == "👨‍👩‍👧‍👧 معرفی به دوستان")
+def refer(message):
+    main_handler.refer(message)
+
+
+ignored_user_ids = [622491665]  # Add the user IDs you want to ignore here
+
+
+@bot.message_handler(func=lambda message: message.from_user.id in ignored_user_ids)
+def ignore_messages(message):
+    pass
