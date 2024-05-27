fc6a576cd726ffa753f0ea4eb7a7159ae10a5465
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-05-27 20:24:13 +0330

    Update run_bot.py

--- core/bot/management/commands/run_bot.py
+++ core/bot/management/commands/run_bot.py
@@ -1,38 +1,13 @@
-import os
-import sys
-import time
-
-from django.core.management import BaseCommand
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 from bot.telegram.handlers.handler import bot
-
-
-class Watcher(FileSystemEventHandler):
-    def __init__(self):
-        self.modified = False
-
-    def on_modified(self, event):
-        if not self.modified:
-            # Check if the modified file is not a JPG or TXT file
-            filename, extension = os.path.splitext(event.src_path)
-            if extension.lower() not in ['.jpg', '.txt']:
-                print("File modified. Reloading bot...")
-                self.modified = True
-                time.sleep(2)  # Add a small delay to prevent rapid restarts
-                os.execv(sys.executable, ["python"] + sys.argv)
+from django.core.management.base import BaseCommand
 
 
 def run_bot():
-    event_handler = Watcher()
-    observer = Observer()
-    observer.schedule(event_handler, path=".", recursive=True)
-    observer.start()
     try:
         bot.infinity_polling()
-    finally:
-        observer.stop()
-        observer.join()
+        print("Bot is running")
+    except KeyboardInterrupt:
+        pass  # Stop the bot gracefully if KeyboardInterrupt is received
 
 
 class Command(BaseCommand):
@@ -40,6 +15,3 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         run_bot()
-
-
-
