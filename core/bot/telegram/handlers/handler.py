b2018857e32960af48b17e2b9b0aae253187e106
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-06-09 14:55:57 +0330

    Update handler.py

--- core/bot/telegram/handlers/handler.py
+++ core/bot/telegram/handlers/handler.py
@@ -10,7 +10,7 @@ from .operations import (
     OrderHandler,
     PurchaseHandler,
     UserHandler,
-    ConfirmationHandler, SubscriptionManager,
+    ConfirmationHandler, 
 )
 from bot.models import BotUser
 from ..utils.funcs import rollback
