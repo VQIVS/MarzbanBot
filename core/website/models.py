5b55e05255ac487336b8b412a29492ee40921329
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-07-01 19:40:48 +0330

    Update models.py

--- core/website/models.py
+++ core/website/models.py
@@ -21,7 +21,7 @@ class Configuration(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField()
+    price = models.DecimalField(decimal_places=0, max_digits=10)
     data_limit = models.FloatField()
     expire = models.IntegerField(blank=True, null=True)
 
