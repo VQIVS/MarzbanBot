7852176e8051d030b1e535f83a24464b4e58b7c5
Mohammad-ali-nazari <mr.mohamadali.nazarii@gmail.com>
2024-07-12 06:30:33 -0700

    Update script.js

--- front/script.js
+++ front/script.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
-    fetch('http://135.181.100.53:8000/swagger/') 
+    fetch('http://135.181.100.53:8000/api/users') 
         .then(response => response.json())
         .then(data => {
             const usersList = document.getElementById('users-list');
+            const userCountElement = document.getElementById('user-count');
 
-            
             data.forEach(user => {
                 const row = document.createElement('tr');
                 row.innerHTML = `
@@ -14,6 +13,10 @@ document.addEventListener('DOMContentLoaded', function() {
                 `;
                 usersList.appendChild(row);
             });
+
+            // Update total users count
+            const totalUsers = data.length;
+            userCountElement.textContent = totalUsers;
         })
         .catch(error => console.error('Error fetching users data:', error));
 });
