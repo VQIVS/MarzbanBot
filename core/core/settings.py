57b268907328b621cd0acf3517e1b5e956b1340e
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2024-08-16 18:04:41 +0330

    update

--- core/core/settings.py
+++ core/core/settings.py
@@ -94,12 +94,8 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'botdb',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': 'marzbanbot-postgres-1',  # Replace with your container name
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
